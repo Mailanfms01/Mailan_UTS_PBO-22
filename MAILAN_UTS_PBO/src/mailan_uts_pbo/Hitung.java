f3e4b39418ef82576bd95756f15e5d5081192e64
Mailanfms01 <mailansholihah01@gmail.com>
2022-05-18 00:43:00 +0700

    Program Menghitung THR

---
+++ MAILAN_UTS_PBO/src/mailan_uts_pbo/Hitung.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mailan_uts_pbo;
+
+/**
+ *
+ * @author ABC
+ */
+public class Hitung {
+    public int gaji;
+    public int kerja;
+    public int thr;
+   
+   public int thr(){
+          double thr = 0;
+        
+        if (kerja >= 12) {
+            thr=gaji;
+        }else
+            if (kerja <12) {
+            thr=(gaji/12)*kerja;
+        }
+        return (int) thr;
+        
+        
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
+
+    public void setGaji(int gaji) {
+        this.gaji = gaji;
+    }
+
+    public int getKerja() {
+        return kerja;
+        
+    }
+
+    public void setKerja(int kerja) {
+        this.kerja = kerja;
+    }
+    
+    public int getThr(){
+       return thr;
+    }
+    public void setThr(int thr) {
+        this.thr = thr;
+        
+    }
+
+    
+    
+}
